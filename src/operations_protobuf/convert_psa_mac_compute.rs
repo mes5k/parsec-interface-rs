f59d1102215f16b9ee7322930c97389625383fa1
Mike Smoot <mes@aescon.com>
2024-02-10 16:37:04 -0800

    added convert_psa_mac_compute tests

--- src/operations_protobuf/convert_psa_mac_compute.rs
+++ src/operations_protobuf/convert_psa_mac_compute.rs
@@ -61,117 +61,142 @@ impl TryFrom<Result> for ResultProto {
     }
 }
 
-//#[cfg(test)]
-//mod test {
-//    use super::super::generated_ops::psa_algorithm as algorithm_proto;
-//    use super::super::generated_ops::psa_hash_compute::{
-//        Operation as OperationProto, Result as ResultProto,
-//    };
-//    use super::super::{Convert, ProtobufConverter};
-//    use crate::operations::psa_algorithm::Hash;
-//    use crate::operations::psa_hash_compute::{Operation, Result};
-//    use crate::operations::{NativeOperation, NativeResult};
-//    use crate::requests::{request::RequestBody, response::ResponseBody, Opcode};
-//    use std::convert::TryInto;
-//
-//    static CONVERTER: ProtobufConverter = ProtobufConverter {};
-//
-//    #[test]
-//    fn hash_compute_proto_to_op() {
-//        let mut proto: OperationProto = Default::default();
-//        let input = vec![0x11, 0x22, 0x33];
-//        proto.input = input.clone();
-//        proto.alg = algorithm_proto::algorithm::Hash::Sha256.into();
-//
-//        let op: Operation = proto.try_into().expect("Failed to convert");
-//
-//        assert_eq!(op.input, input.into());
-//        assert_eq!(op.alg, Hash::Sha256);
-//    }
-//
-//    #[test]
-//    fn hash_compute_op_to_proto() {
-//        let input = vec![0x11, 0x22, 0x33];
-//        let alg = Hash::Sha256;
-//
-//        let op = Operation {
-//            alg,
-//            input: input.clone().into(),
-//        };
-//
-//        let proto: OperationProto = op.try_into().expect("Failed to convert");
-//
-//        assert_eq!(proto.input, input);
-//    }
-//
-//    #[test]
-//    fn hash_compute_proto_to_resp() {
-//        let mut proto: ResultProto = Default::default();
-//        let hash = vec![0x11, 0x22, 0x33];
-//        proto.hash = hash.clone();
-//
-//        let result: Result = proto.try_into().expect("Failed to convert");
-//
-//        assert_eq!(result.hash, hash.into());
-//    }
-//
-//    #[test]
-//    fn hash_compute_resp_to_proto() {
-//        let hash = vec![0x11, 0x22, 0x33];
-//        let result = Result {
-//            hash: hash.clone().into(),
-//        };
-//
-//        let proto: ResultProto = result.try_into().expect("Failed to convert");
-//
-//        assert_eq!(proto.hash, hash);
-//    }
-//
-//    #[test]
-//    fn op_hash_compute_sign_e2e() {
-//        let op = Operation {
-//            input: vec![0x11, 0x22, 0x33].into(),
-//            alg: Hash::Sha256,
-//        };
-//        let body = CONVERTER
-//            .operation_to_body(NativeOperation::PsaHashCompute(op))
-//            .expect("Failed to convert request");
-//
-//        assert!(CONVERTER
-//            .body_to_operation(body, Opcode::PsaHashCompute)
-//            .is_ok());
-//    }
-//
-//    #[test]
-//    fn resp_hash_compute_sign_e2e() {
-//        let result = Result {
-//            hash: vec![0x11, 0x22, 0x33].into(),
-//        };
-//        let body = CONVERTER
-//            .result_to_body(NativeResult::PsaHashCompute(result))
-//            .expect("Failed to convert request");
-//
-//        assert!(CONVERTER
-//            .body_to_result(body, Opcode::PsaHashCompute)
-//            .is_ok());
-//    }
-//
-//    #[test]
-//    fn result_from_mangled_resp_body() {
-//        let resp_body =
-//            ResponseBody::from_bytes(vec![0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88]);
-//        assert!(CONVERTER
-//            .body_to_result(resp_body, Opcode::PsaHashCompute)
-//            .is_err());
-//    }
-//
-//    #[test]
-//    fn op_from_mangled_req_body() {
-//        let req_body =
-//            RequestBody::from_bytes(vec![0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88]);
-//
-//        assert!(CONVERTER
-//            .body_to_operation(req_body, Opcode::PsaHashCompute)
-//            .is_err());
-//    }
-//}
+#[cfg(test)]
+mod test {
+    use super::super::generated_ops::psa_algorithm as algorithm_proto;
+    use super::super::generated_ops::psa_mac_compute::{
+        Operation as OperationProto, Result as ResultProto,
+    };
+    use super::super::{Convert, ProtobufConverter};
+    use crate::operations::psa_mac_compute::{Operation, Result};
+    use crate::operations::{NativeOperation, NativeResult};
+    use crate::requests::{request::RequestBody, response::ResponseBody, Opcode};
+    use std::convert::TryInto;
+
+    static CONVERTER: ProtobufConverter = ProtobufConverter {};
+
+    fn make_mac() -> Option<algorithm_proto::algorithm::Mac> {
+        Some(algorithm_proto::algorithm::Mac {
+            variant: Some(algorithm_proto::algorithm::mac::Variant::Truncated(
+                algorithm_proto::algorithm::mac::Truncated {
+                    mac_length: 32,
+                    mac_alg: Some(algorithm_proto::algorithm::mac::FullLength {
+                        variant: Some( algorithm_proto::algorithm::mac::full_length::Variant::Hmac (
+                            algorithm_proto::algorithm::mac::full_length::Hmac {
+                                hash_alg: algorithm_proto::algorithm::Hash::Sha256.into()
+                            }
+                        ))
+                    }),
+                }
+            ))
+        })
+    }
+
+    #[test]
+    fn mac_compute_proto_to_op() {
+        let key_name = "test".to_string();
+        let input = vec![0x11, 0x22, 0x33];
+        let alg = make_mac();
+
+        let mut proto: OperationProto = Default::default();
+        proto.key_name = key_name.clone();
+        proto.input = input.clone();
+        proto.alg = alg.clone();
+
+        let op: Operation = proto.try_into().expect("Failed to convert");
+
+        assert_eq!(op.input, input.into());
+        assert_eq!(op.alg, alg.unwrap().try_into().unwrap());
+    }
+
+    #[test]
+    fn mac_compute_op_to_proto() {
+        let key_name = "test".to_string();
+        let input = vec![0x11, 0x22, 0x33];
+        let alg = make_mac();
+
+        let op = Operation {
+            key_name: key_name.clone().into(),
+            alg: alg.clone().unwrap().try_into().unwrap(),
+            input: input.clone().into(),
+        };
+
+        let proto: OperationProto = op.try_into().expect("Failed to convert");
+
+        assert_eq!(proto.input, input);
+    }
+
+    #[test]
+    fn mac_compute_proto_to_resp() {
+        let mac = vec![0x11, 0x22, 0x33];
+
+        let mut proto: ResultProto = Default::default();
+        proto.mac = mac.clone();
+
+        let result: Result = proto.try_into().expect("Failed to convert");
+
+        assert_eq!(result.mac, mac.into());
+    }
+
+    #[test]
+    fn mac_compute_resp_to_proto() {
+        let mac = vec![0x11, 0x22, 0x33];
+        let result = Result {
+            mac: mac.clone().into(),
+        };
+
+        let proto: ResultProto = result.try_into().expect("Failed to convert");
+
+        assert_eq!(proto.mac, mac);
+    }
+
+    #[test]
+    fn op_hash_compute_sign_e2e() {
+        let alg = make_mac();
+        let op = Operation {
+            key_name: "test".to_string().into(),
+            input: vec![0x11, 0x22, 0x33].into(),
+            alg: alg.clone().unwrap().try_into().unwrap(),
+        };
+        let body = CONVERTER
+            .operation_to_body(NativeOperation::PsaMacCompute(op))
+            .expect("Failed to convert request");
+
+        assert!(CONVERTER
+            .body_to_operation(body, Opcode::PsaMacCompute)
+            .is_ok());
+    }
+
+    #[test]
+    fn resp_mac_compute_sign_e2e() {
+        let result = Result {
+            mac: vec![0x11, 0x22, 0x33].into(),
+        };
+        let body = CONVERTER
+            .result_to_body(NativeResult::PsaMacCompute(result))
+            .expect("Failed to convert request");
+
+        assert!(CONVERTER
+            .body_to_result(body, Opcode::PsaMacCompute)
+            .is_ok());
+    }
+
+    #[test]
+    fn result_from_mangled_resp_body() {
+        let resp_body =
+            ResponseBody::from_bytes(vec![0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88]);
+        assert!(CONVERTER
+            .body_to_result(resp_body, Opcode::PsaMacCompute)
+            .is_err());
+    }
+
+    #[test]
+    fn op_from_mangled_req_body() {
+        let req_body =
+            RequestBody::from_bytes(vec![0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88]);
+
+        assert!(CONVERTER
+            .body_to_operation(req_body, Opcode::PsaMacCompute)
+            .is_err());
+    }
+}
