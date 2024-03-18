6d13e4dd58801217960fccc04557cb1f46052b36
Mike Smoot <mes@aescon.com>
2024-03-18 08:50:13 -0700

    added tests

--- src/operations/psa_mac_compute.rs
+++ src/operations/psa_mac_compute.rs
@@ -25,11 +25,14 @@ impl Operation {
     /// Validate the contents of the operation against the attributes of the key it targets
     ///
     /// This method checks that:
+    /// * the key policy usage allows signing of the hash
     /// * the key policy allows the encryption algorithm requested in the operation
     /// * there is sufficient buffer size for the MAC of the specified algorithm, if compatible
     pub fn validate(&self, key_attributes: Attributes) -> crate::requests::Result<()> {
+        key_attributes.can_sign_hash()?;
         key_attributes.permits_alg(self.alg.into())?;
-        let _size = key_attributes.mac_length(self.alg.into())?;
+        let size = key_attributes.mac_length(self.alg.into())?;
+        println!("SIZE: {}",size);
         Ok(())
     }
 }
@@ -41,4 +44,92 @@ pub struct Result {
     /// The `mac` field contains the MAC value of the message.
     #[derivative(Debug = "ignore")]
     pub mac: zeroize::Zeroizing<Vec<u8>>,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::operations::psa_algorithm::{Algorithm, Mac, FullLengthMac, Hash};
+    use crate::operations::psa_key_attributes::{Lifetime, Policy, Type, UsageFlags};
+    use crate::requests::ResponseStatus;
+
+    fn get_attrs(mac: Mac) -> Attributes {
+        let permitted_alg = Algorithm::Mac(mac);
+        // UsageFlags defined in psa-cryto/src/types/keys.rs, gets re-exported by parsec-client-rust
+        let mut usage = UsageFlags::default();
+        // set_sign_hash is needed for mac_compute, set_verify_hash is needed for mac_verify
+        let _ = usage.set_sign_hash().set_verify_hash();
+        // Attributes defined in psa-cryto/src/types/keys.rs, gets re-exported by parsec-client-rust
+        let attributes = Attributes {
+            key_type: Type::Hmac,
+            bits: 256,
+            lifetime: Lifetime::Volatile,
+            policy: Policy {
+                usage_flags: usage,
+                permitted_algorithms: permitted_alg,
+            },
+        };
+        attributes
+    }
+
+    #[test]
+    fn validate_success() {
+        let alg = Mac::FullLength(FullLengthMac::Hmac { hash_alg: Hash::Sha256 });
+        (Operation {
+            key_name: String::from("some key"),
+            alg,
+            input: vec![0xff; 32].into(),
+        })
+        .validate(get_attrs(alg))
+        .unwrap();
+    }
+
+    #[test]
+    fn cannot_sign() {
+        let alg = Mac::FullLength(FullLengthMac::Hmac { hash_alg: Hash::Sha256 });
+        let mut attrs = get_attrs(alg);
+        attrs.policy.usage_flags = UsageFlags::default();
+        assert_eq!(
+            (Operation {
+                key_name: String::from("some key"),
+                alg,
+                input: vec![0xff; 32].into(),
+            })
+            .validate(attrs)
+            .unwrap_err(),
+            ResponseStatus::PsaErrorNotPermitted
+        );
+    }
+
+    #[test]
+    fn wrong_algorithm() {
+        let right = Mac::FullLength(FullLengthMac::Hmac { hash_alg: Hash::Sha1 });
+        let wrong = Mac::FullLength(FullLengthMac::Hmac { hash_alg: Hash::Sha256 });
+        assert_eq!(
+            (Operation {
+                key_name: String::from("some key"),
+                alg: right,
+                input: vec![0xff; 32].into(),
+            })
+            .validate(get_attrs(wrong))
+            .unwrap_err(),
+            ResponseStatus::PsaErrorNotPermitted
+        );
+    }
+
+//    #[test]
+//    fn bad_mac_length() {
+//        let alg = Mac::FullLength(FullLengthMac::Hmac { hash_alg: Hash::Sha1 });
+//        let attrs = get_attrs(alg);
+//        assert_eq!(
+//            (Operation {
+//                key_name: String::from("some key"),
+//                alg,
+//                input: vec![0xff; 32].into(),
+//            })
+//            .validate(attrs)
+//            .unwrap_err(),
+//            ResponseStatus::PsaErrorNotPermitted
+//        );
+//    }
 }
--- src/operations/psa_mac_verify.rs
+++ src/operations/psa_mac_verify.rs
@@ -28,9 +28,11 @@ impl Operation {
     /// Validate the contents of the operation against the attributes of the key it targets
     ///
     /// This method checks that:
+    /// * the key policy usage allows hash to be verified
     /// * the key policy allows the encryption algorithm requested in the operation
     /// * there is sufficient buffer size for the MAC of the specified algorithm, if compatible
     pub fn validate(&self, key_attributes: Attributes) -> crate::requests::Result<()> {
+        key_attributes.can_verify_hash()?;
         key_attributes.permits_alg(self.alg.into())?;
         let _size = key_attributes.mac_length(self.alg.into())?;
         Ok(())
@@ -40,3 +42,79 @@ impl Operation {
 /// Native object for MAC verify result.
 #[derive(Debug, Default, Copy, Clone)]
 pub struct Result;
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::operations::psa_algorithm::{Algorithm, Mac, FullLengthMac, Hash};
+    use crate::operations::psa_key_attributes::{Lifetime, Policy, Type, UsageFlags};
+    use crate::requests::ResponseStatus;
+
+    fn get_attrs(mac: Mac) -> Attributes {
+        let permitted_alg = Algorithm::Mac(mac);
+        // UsageFlags defined in psa-cryto/src/types/keys.rs, gets re-exported by parsec-client-rust
+        let mut usage = UsageFlags::default();
+        // set_sign_hash is needed for mac_compute, set_verify_hash is needed for mac_verify
+        let _ = usage.set_sign_hash().set_verify_hash();
+        // Attributes defined in psa-cryto/src/types/keys.rs, gets re-exported by parsec-client-rust
+        let attributes = Attributes {
+            key_type: Type::Hmac,
+            bits: 256,
+            lifetime: Lifetime::Volatile,
+            policy: Policy {
+                usage_flags: usage,
+                permitted_algorithms: permitted_alg,
+            },
+        };
+        attributes
+    }
+
+    #[test]
+    fn validate_success() {
+        let alg = Mac::FullLength(FullLengthMac::Hmac { hash_alg: Hash::Sha256 });
+        (Operation {
+            key_name: String::from("some key"),
+            alg,
+            input: vec![0xff; 32].into(),
+            mac: vec![0xff; 32].into(),
+        })
+        .validate(get_attrs(alg))
+        .unwrap();
+    }
+
+    #[test]
+    fn cannot_verify() {
+        let alg = Mac::FullLength(FullLengthMac::Hmac { hash_alg: Hash::Sha256 });
+        let mut attrs = get_attrs(alg);
+        attrs.policy.usage_flags = UsageFlags::default();
+        assert_eq!(
+            (Operation {
+                key_name: String::from("some key"),
+                alg,
+                input: vec![0xff; 32].into(),
+                mac: vec![0xff; 32].into(),
+            })
+            .validate(attrs)
+            .unwrap_err(),
+            ResponseStatus::PsaErrorNotPermitted
+        );
+    }
+
+    #[test]
+    fn wrong_algorithm() {
+        let right = Mac::FullLength(FullLengthMac::Hmac { hash_alg: Hash::Sha1 });
+        let wrong = Mac::FullLength(FullLengthMac::Hmac { hash_alg: Hash::Sha256 });
+        assert_eq!(
+            (Operation {
+                key_name: String::from("some key"),
+                alg: right,
+                input: vec![0xff; 32].into(),
+                mac: vec![0xff; 32].into(),
+            })
+            .validate(get_attrs(wrong))
+            .unwrap_err(),
+            ResponseStatus::PsaErrorNotPermitted
+        );
+    }
+}
